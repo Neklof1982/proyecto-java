ef9249a4bc301a0b67df4d249355655806d91021
amargra329 <amargra329@g.educaand.es>
2024-05-07 08:28:06 +0200

    8:27 07/05/2024

---
+++ src/back/Nota.java
@@ -0,0 +1,36 @@
+package back;
+
+public class Nota {
+
+    // #### ATRIBUTOS #################
+
+    private final int id;
+    private String nombre;
+    private String nota;
+
+    // #### CONSTRUCTOR ###############
+
+    public Nota(int id, String nombre, String nota) {
+        this.id = id;
+        this.nombre = nombre;
+        this.nota = nota;
+    }
+
+    // #### GET and SET ###############
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNota() {
+        return nota;
+    }
+
+    public void setNota(String nota) {
+        this.nota = nota;
+    }
+}
---
+++ src/front/Editor.java
@@ -0,0 +1,16 @@
+package front;
+
+import javax.swing.*;
+
+public class Editor extends JFrame {
+    
+    private JPanel editor;
+    private JButton upload;
+    private JToolBar tollbar;
+    private JList notes;
+    private JTextArea textEditor;
+
+    public Editor() {
+
+    }
+}
