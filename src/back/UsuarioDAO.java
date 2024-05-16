91de102852e48b5486d9640a747412c95febbc0e
omarsan3001 <omarsan3001@g.educaand.es>
2024-05-16 08:56:58 +0200

    8:56 16/05/2024

--- src/back/UsuarioDAO.java
+++ src/back/UsuarioDAO.java
@@ -1,24 +1,31 @@
 package back;
 
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+
+import java.io.*;
 import java.sql.*;
 import java.util.Iterator;
 
 public class UsuarioDAO {
 
-    public static Usuario obtenerUsuario(String nombre, String contraseña) {
+    public static Usuario obtenerUsuario() {
         Connection con;
         PreparedStatement statement;
         ResultSet rs;
         String url = "jdbc:mysql://localhost/Proyecto";
-        Usuario usuario = new Usuario(contraseña, nombre);
+        // Usuario usuario = new Usuario(contraseña, nombre);
+        Usuario usuario = null;
         int contador = 0;
 
+        usuario = getUsuario();
+
         try {
-            con = DriverManager.getConnection(url, "Pepe", "12345");
+            con = getConnection(url);
             String consulta = "SELECT COUNT(*) , apellidos FROM usuario WHERE id_nombre = ? AND contrasenia = ?";
             statement = con.prepareStatement(consulta);
-            statement.setString(1, nombre);
-            statement.setString(2, contraseña);
+            statement.setString(1, usuario.getNombre());
+            statement.setString(2, usuario.getContraseña());
             rs = statement.executeQuery();
             while (rs.next()) {
                 contador += rs.getInt(1);
@@ -29,7 +36,7 @@ public class UsuarioDAO {
             }
             consulta = "select * from notas where id_nombreFK = ?";
             statement = con.prepareStatement(consulta);
-            statement.setString(1, nombre);
+            statement.setString(1, usuario.getNombre());
             rs = statement.executeQuery();
             while (rs.next()) {
                 usuario.getListaNotas().add(new Nota(rs.getInt(1), rs.getString(2), rs.getString(4),rs.getString(3),rs.getInt(5)));
@@ -46,19 +53,23 @@ public class UsuarioDAO {
         return usuario;
     }
 
-    public static boolean crearUsuario(String nombre,String apellido, String contraseña) {
+    public static boolean crearUsuario() {
         Connection con;
         PreparedStatement statement;
         String consulta;
         String url = "jdbc:mysql://localhost/Proyecto";
 
+        Usuario usuario = null;
+
+        usuario = getUsuario();
+
         try {
-            con = DriverManager.getConnection(url, "Pepe", "12345");
+            con = getConnection(url);
             consulta = "insert into usuario(id_nombre, apellidos, contrasenia) values (?,?,?)";
             statement = con.prepareStatement(consulta);
-            statement.setString(1, nombre);
-            statement.setString(2, apellido);
-            statement.setString(3, contraseña);
+            statement.setString(1, usuario.getNombre());
+            statement.setString(2, usuario.getApellido());
+            statement.setString(3, usuario.getContraseña());
             statement.executeUpdate();
         } catch (SQLException e) {
             return false;
@@ -67,18 +78,21 @@ public class UsuarioDAO {
         return true;
     }
 
-    public static boolean borrarUsuario(String nombre, String contraseña){
+    public static boolean borrarUsuario(){
         Connection con;
         PreparedStatement statement;
         String consulta;
         String url = "jdbc:mysql://localhost/Proyecto";
+        Usuario usuario = null;
+
+        usuario = getUsuario();
 
         try {
-            con = DriverManager.getConnection(url, "Pepe", "12345");
+            con = getConnection(url);
             consulta = "delete from usuario where id_nombre = ? and contrasenia = ?";
             statement = con.prepareStatement(consulta);
-            statement.setString(1, nombre);
-            statement.setString(2, contraseña);
+            statement.setString(1, usuario.getNombre());
+            statement.setString(2, usuario.getContraseña());
             statement.executeUpdate();
         } catch (SQLException e) {
             return false;
@@ -87,14 +101,18 @@ public class UsuarioDAO {
         return true;
     }
 
-    public static boolean actualizarNotas(Usuario usu){
+    public static boolean actualizarNotas(){
         Connection con;
         PreparedStatement statement;
         String consulta;
         String url = "jdbc:mysql://localhost/Proyecto";
 
+        Usuario usu = null;
+
+        usu = getUsuario();
+
         try {
-            con = DriverManager.getConnection(url, "Pepe", "12345");
+            con = getConnection(url);
             consulta = "delete from notas where id_nombreFK = ?";
             statement = con.prepareStatement(consulta);
             statement.setString(1, usu.getNombre());
@@ -117,5 +135,49 @@ public class UsuarioDAO {
         }
 
         return true;
+    }
+
+    private static Connection getConnection(String url) throws SQLException {
+
+        String usuario = null;
+        String pass = null;
+        String[] array = null;
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("conexion.txt"));
+
+            array = bufferedReader.readLine().split(",");
+
+            usuario = array[0];
+            pass = array[1];
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        return DriverManager.getConnection(url, usuario, pass);
+    }
+
+    private static Usuario getUsuario() {
+        Usuario usu = null;
+
+        File datosUsuariosR = new File("entrada.json");
+
+        if (datosUsuariosR.exists()) {
+            try {
+                Gson gson = new Gson();
+
+                FileReader lector = new FileReader("entrada.json");
+                JsonReader lectorJson = new JsonReader(lector);
+
+                usu = gson.fromJson(lectorJson, Usuario.class);
+
+                lectorJson.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return usu;
     }
 }
